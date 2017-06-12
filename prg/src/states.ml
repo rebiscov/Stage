eff8d1816a6f2dbf4b3f4a670521eca7498959f5
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-12 15:06:57 +0200

    simplified the algo

--- prg/src/states.ml
+++ prg/src/states.ml
@@ -7,17 +7,14 @@ let compute_next_w w s d =
   let ret = ref 1 in
   let i = ref 0 in
   let sigma_i = ref 0 in
-  let sigma_i_prev = ref 0 in
   while !i < d && !ret = 1 do
     sigma_i := !sigma_i+w.(!i);
     ret := (!sigma_i+1)/((!i+1)*s+1);
     sigma_i := (!sigma_i+1)mod((!i+1)*s+1);
-    w.(!i) <- !sigma_i - !sigma_i_prev;
-    sigma_i_prev := !sigma_i;
+    w.(!i) <- !sigma_i;
     i := !i+1
   done;
   w
-
 
   
 
