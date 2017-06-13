56347185a03951026eca7de68d3af0de81a53f7f
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-13 15:23:47 +0200

    adding sigs of set and get funs

--- prg/src/states.mli
+++ prg/src/states.mli
@@ -1,7 +1,13 @@
-type w = int array
+type w
 
 (** Construct the state 0 *)
 val new_w : int -> w 
 
 (** Given a state, it computes the next state *)
 val compute_next_w : w -> int -> int -> w
+
+(** give the value of w[i], w begin the remaining work function *)
+val get : w -> int -> int
+
+(** set the value of w[i], w begin the remaining work function *)
+val set : w -> int -> int -> unit
