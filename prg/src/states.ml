4debff39a084e0482d818afb9e086337d5f866c2
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-13 10:37:31 +0200

    changing the order of the partial sum

--- prg/src/states.ml
+++ prg/src/states.ml
@@ -4,16 +4,16 @@ let new_w d =
   Array.make d 0
 
 let rec compute_next_w_i w i d s =
-  w.(i) <- (w.(i)+1)mod((i+1)*s+1);
-  if w.(i) = 0 && i > 0 then
+  w.(i) <- (w.(i)+1)mod((d-i)*s+1);
+  if w.(i) = 0 && i < d-1 then
     begin
-      w.(i) <- compute_next_w_i w (i-1) d s;
+      w.(i) <- compute_next_w_i w (i+1) d s;
       w.(i)
     end
   else
     w.(i)
 
 let compute_next_w w d s =
-  let _ = compute_next_w_i w (d-1) d s in
+  let _ = compute_next_w_i w 0 d s in
   w
         
