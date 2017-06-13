9d3cc72f75e9165bc96546c1b5cd4aa37b248c34
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-13 16:29:44 +0200

    add functions add and inc_time

--- prg/src/states.ml
+++ prg/src/states.ml
@@ -3,6 +3,9 @@ type w = int array
 let new_w d =
   Array.make d 0
 
+let copy w =
+  Array.copy w
+
 let rec compute_next_w_i w i d s =
   w.(i) <- (w.(i)+1)mod((i+1)*s+1);
   if w.(i) = 0 && i > 0 then
@@ -15,11 +18,33 @@ let rec compute_next_w_i w i d s =
 
 let compute_next_w w d s =
   let _ = compute_next_w_i w (d-1) d s in
-  w
+  ()
 
 let get w i =
   if i = 0 then
     w.(0)
   else
-    w.(i) - w.(i-1)
+    w.(i)
 
+let add w i e =
+  let d = Array.length w in
+  let rec set_aux w i e =
+    if i < d then
+      begin
+        w.(i) <- w.(i) + e;
+        set_aux w (i+1) e
+      end
+    else
+      () in
+  set_aux w i e
+
+let inc_time w v =
+  let d = Array.length w in
+  for i=0 to d-2 do
+    w.(i) <- max (w.(i+1)-v) 0;
+  done;
+  w.(d-1) <- 0;
+  ()
+    
+  
+    
--- prg/src/states.mli
+++ prg/src/states.mli
@@ -1,13 +1,20 @@
 type w
 
-(** Construct the state 0 *)
+(** Construct the state 0 (the function always equals to zero *)
 val new_w : int -> w 
 
+(** Copy a remaining work function *)
+val copy : w -> w
+  
 (** Given a state, it computes the next state *)
-val compute_next_w : w -> int -> int -> w
+val compute_next_w : w -> int -> int -> unit
 
-(** give the value of w[i], w begin the remaining work function *)
+(** Give the value of w[i], w being the remaining work function *)
 val get : w -> int -> int
 
-(** set the value of w[i], w begin the remaining work function *)
-val set : w -> int -> int -> unit
+(** add some value starting at i (you're incrementing the step between w[i] and w[i+1]) *)
+val add : w -> int -> int -> unit
+
+(** Computes w_t from w_t-1 with s=0 and d=0 and v provided by the user (see Lemma 1 of the paper) *)
+val inc_time : w -> int -> unit
+
