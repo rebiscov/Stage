255336f44776963575979af1d2a32741d7bf903d
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-13 10:21:46 +0200

    First version of the working algo

--- prg/src/states.ml
+++ prg/src/states.ml
@@ -2,19 +2,18 @@ type w = int array
 
 let new_w d =
   Array.make d 0
-       
-let compute_next_w w s d =
-  let ret = ref 1 in
-  let i = ref 0 in
-  let sigma_i = ref 0 in
-  while !i < d && !ret = 1 do
-    sigma_i := !sigma_i+w.(!i);
-    ret := (!sigma_i+1)/((!i+1)*s+1);
-    sigma_i := (!sigma_i+1)mod((!i+1)*s+1);
-    w.(!i) <- !sigma_i;
-    i := !i+1
-  done;
+
+let rec compute_next_w_i w i d s =
+  w.(i) <- (w.(i)+1)mod((i+1)*s+1);
+  if w.(i) = 0 && i > 0 then
+    begin
+      w.(i) <- compute_next_w_i w (i-1) d s;
+      w.(i)
+    end
+  else
+    w.(i)
+
+let compute_next_w w d s =
+  let _ = compute_next_w_i w (d-1) d s in
   w
-
-  
-
+        
