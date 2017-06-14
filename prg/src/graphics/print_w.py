78b12b4ddf5b718cb54f4006704bf57b29b0be6e
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-14 15:58:44 +0200

    finish the script which print the remaining functions

--- prg/src/graphics/print_w.py
+++ prg/src/graphics/print_w.py
@@ -3,25 +3,26 @@ import matplotlib
 import matplotlib.pyplot as plt
 import sys
     
-if len(sys.argv) < 2:
-    print("Use: ./" + sys.argv[0] + " filename")
+if len(sys.argv) < 4:
+    print("Use: ./" + sys.argv[0] + " d s filename")
     exit(1) 
 
 lines = []
-with open(sys.argv[1]) as file:
+with open(sys.argv[3]) as file:
     lines = file.readlines()
 
 num = []
 for line in lines:
     num.append(list(map(int,line.split())))
 
-print(num)
-    
-    
-time = [0,1,2,3]
-value = [0,2,4,5]
+d = int(sys.argv[1])
+s = int(sys.argv[2])
+x = np.linspace(0,d+1,d+2)
 
-plt.step(time, value, where='post')
+for y in num:
+    plt.xlim(0,d+1)
+    plt.ylim(0,d*s)
+    plt.step(x, y + [y[-1]], where='post')
+    plt.show()
 
-plt.show()
 
