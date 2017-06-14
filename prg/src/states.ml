8e3d95513220faffbbcfc3a847221c4c6174a8c7
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-14 10:34:50 +0200

    minor changes

--- prg/src/states.ml
+++ prg/src/states.ml
@@ -25,11 +25,11 @@ let get w i =
 
 let add w i e =
   let d = Array.length w in
-  let rec set_aux w i e =
+  let rec add_aux w i e =
     if i < d then
       begin
         w.(i) <- w.(i) + e;
-        set_aux w (i+1) e
+        add_aux w (i+1) e
       end
     else
       () in
