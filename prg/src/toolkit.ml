e16bacf459ca7731c072e78641304ef76122f472
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-14 15:31:39 +0200

    toolkit almost finished, need debug

--- prg/src/toolkit.ml
+++ prg/src/toolkit.ml
@@ -6,20 +6,30 @@ let write file w d =
   for i=0 to d-1 do
     fprintf file "%d " (S.get w i)
   done;
-  fprintf file "%d" (S.get w d)
+  fprintf file "%d\n" (S.get w d)
 
 let () =
   printf "Welcome to the toolkit\n";
-  if (Array.length Sys.argv < 3) then
+  if (Array.length Sys.argv < 4) then
     begin
-      printf "Use: %s d s\n" Sys.argv.(0);
+      printf "Use: %s d s filename\n" Sys.argv.(0);
       exit 1
     end
   else
     ();
   let d = int_of_string Sys.argv.(1) in
   let s = int_of_string Sys.argv.(2) in
+  let filename = Sys.argv.(3) in
+  let file = open_out filename in
+  let w = S.new_w d in
+  
+  S.compute_next_w w d s;
+  while not (S.is_null w) do
+    write file w d;
+  done;
+  close_out file;
   ()
+  
   
   
 
