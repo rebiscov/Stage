fb4050648300fd525100846e8e345632253f9a56
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-14 15:39:24 +0200

    the toolkit is now working

--- prg/src/toolkit.ml
+++ prg/src/toolkit.ml
@@ -22,12 +22,15 @@ let () =
   let filename = Sys.argv.(3) in
   let file = open_out filename in
   let w = S.new_w d in
-  
+  let count = ref 1 in
   S.compute_next_w w d s;
   while not (S.is_null w) do
     write file w d;
+    count := !count + 1;
+    S.compute_next_w w d s;
   done;
   close_out file;
+  printf "%d functions generated with d=%d and s=%d\n" !count d s;
   ()
   
   
