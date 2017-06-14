65225754a7905561fe91f06515fe9ccbda629845
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-14 09:13:08 +0200

    reorganizing comments

--- prg/src/states.mli
+++ prg/src/states.mli
@@ -1,20 +1,19 @@
 type w
 
+val new_w : int -> w
 (** Construct the state 0 (the function always equals to zero *)
-val new_w : int -> w 
 
-(** Copy a remaining work function *)
 val copy : w -> w
+(** Copy a remaining work function *)  
   
-(** Given a state, it computes the next state *)
 val compute_next_w : w -> int -> int -> unit
+(** Given a state, it computes the next state *)
 
-(** Give the value of w[i], w being the remaining work function *)
 val get : w -> int -> int
+(** Give the value of w[i], w being the remaining work function *)
 
-(** add some value starting at i (you're incrementing the step between w[i] and w[i+1]) *)
 val add : w -> int -> int -> unit
+(** add some value starting at i (you're incrementing the step between w[i] and w[i+1]) *)
 
-(** Computes w_t from w_t-1 with s=0 and d=0 and v provided by the user (see Lemma 1 of the paper) *)
 val inc_time : w -> int -> unit
-
+(** Computes w_t from w_t-1 with s=0 and d=0 and v provided by the user (see Lemma 1 of the paper) *)
