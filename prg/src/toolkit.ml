316faed6c71ece0ee553d856e11d2358fc854ff3
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-14 15:14:22 +0200

    adding args management

--- prg/src/toolkit.ml
+++ prg/src/toolkit.ml
@@ -1,4 +1,5 @@
 open Printf
+open Sys
 module S = States
 
 let write file w d =
@@ -6,5 +7,18 @@ let write file w d =
     fprintf file "%d " (S.get w i)
   done;
   fprintf file "%d" (S.get w d)
+
+let () =
+  printf "Welcome to the toolkit\n";
+  if (Array.length Sys.argv < 3) then
+    begin
+      printf "Use: %s d s\n" Sys.argv.(0);
+      exit 1
+    end
+  else
+    ();
+  printf "yo\n"
+
+  
 
   
