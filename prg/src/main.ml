2c21b88f927c389ec151da37070ea65a571983cb
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-15 11:28:25 +0200

    began to code main

--- prg/src/main.ml
+++ prg/src/main.ml
@@ -1,6 +1,16 @@
 open Printf
+module S = States
 
 let () =
-  Printf.printf "DP algorithm, first version\n"
+  printf "DP algorithm, first version\n";
+  if (Array.length Sys.argv < 3) then
+    begin
+      printf "Use: %s T distribustion_file options\n" Sys.argv.(0);
+      exit 1
+    end
+  else
+    ();
+  let t = int_of_string Sys.argv.(1) in
+  
   
    
