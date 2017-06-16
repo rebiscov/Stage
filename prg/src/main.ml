e5b66aa2925f4473fc3ecf2ae93357ce4f42e202
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-16 09:49:56 +0200

    init all objects needed for main algo

--- prg/src/main.ml
+++ prg/src/main.ml
@@ -20,13 +20,18 @@ let () =
   (* Opening the file containing the distribution and extracting deadline limit and workload limit variables, defining state space *)
   let fd = open_in Sys.argv.(3) in
   let ffd = (Scanf.Scanning.from_channel fd) in
-  let (d, s) = Scanf.bscanf ffd "%d %d" (fun x y -> (x,y)) in
+  let (d, s) = Scanf.bscanf ffd "%d %d\n" (fun x y -> (x,y)) in
   let space = S.state_space d s in
 
   (* Defining the hashtable, the distribution and the array of the expected consumption *)
   let h = Hashtbl.create space in
   
-  let distribution = Array.make_matrix space (d*s) 0. in
+  let distribution = Array.make_matrix space d [||] in
+  for i=0 to space-1 do
+    for j=0 to d-1 do
+      distribution.(i).(j) <- Array.make s 0.
+    done;
+  done;
   
   let opt = Array.make_matrix space (bt+1) [||] in
   for i=0 to space - 1 do
@@ -41,13 +46,16 @@ let () =
   let b = ref true in
   while !b do
     Hashtbl.add h w !count;
-    for i=1 to s do
-      for j=d to 1 do
-        distribution.(!count).((i-1)*(j-1)) <- Scanf.bscanf ffd "%f" (fun x -> x)
+    for i=1 to d do
+      for j=1 to s do
+          distribution.(!count).(i-1).(j-1) <- Scanf.bscanf ffd "%f " (fun x -> x)
       done;
     done;
+    count := !count + 1;
     S.compute_next_w w d s;
     if S.is_null w then
       b := false
   done;
+
+  
   ()
