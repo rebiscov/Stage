917696c440b7f0d3c37d21ef39677a4a32d982e0
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-19 14:23:01 +0200

    adapting toolkit to the new compute_w fun

--- prg/src/toolkit.ml
+++ prg/src/toolkit.ml
@@ -21,16 +21,14 @@ let () =
   let s = int_of_string Sys.argv.(2) in
   let filename = Sys.argv.(3) in
   let file = open_out filename in
-  let w = S.new_w d in
-  let count = ref 1 in
-  S.compute_next_w w d s;
-  while not (S.is_null w) do
-    write file w d;
-    count := !count + 1;
-    S.compute_next_w w d s;
+  let space = S.state_space d s in
+  let w_set = S.compute_w d s in
+  
+  for i=0 to space-1 do
+    write file w_set.(i) d;
   done;
   close_out file;
-  printf "%d functions generated with d=%d and s=%d\n" !count d s;
+  printf "%d functions generated with d=%d and s=%d\n" space d s;
   ()
   
   
