0ab608711788cba3be0d157563068c1959f6e06a
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-20 14:20:49 +0200

    finish the programm who prints the results of a simulation

---
+++ prg/src/simu/print_simu.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) < 2:
+    print("Use: ./" + sys.argv[0] + " values_file")
+    exit(1)
+    
+lines = []
+with open(sys.argv[1]) as file:
+    lines = file.readlines()
+
+num = []
+for line in lines:
+    num.append(list(map(int,line.split())))
+
+t = len(num[0])
+x = np.linspace(0, t-1, t)
+
+plt.xlim(0,t)
+plt.plot(x, num[0])
+plt.plot(x, num[1])
+plt.show()
