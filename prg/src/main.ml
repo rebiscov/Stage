f055e9b2492d728bd876ae9d7bf2d457bb318b6d
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-20 09:16:44 +0200

    the case t=0 is now treated like the other cases

--- prg/src/main.ml
+++ prg/src/main.ml
@@ -6,15 +6,15 @@ let compute_sum w'' d s v opt distribution h t = (* Computes the sum in the main
   S.inc_time w v;
   let id_w = Hashtbl.find h w in
   let sum = ref 0. in
-  for i=1 to s do
+  for i=0 to s do
     let w' = S.copy w in
     for j=1 to d do
       let k = d-j+1 in
       S.set w' k (s + S.get w' k);
-      sum := !sum +. distribution.(id_w).(k-1).(i-1) *. opt.(t+1).(Hashtbl.find h w').(v);
+      sum := !sum +. distribution.(id_w).(k-1).(i) *. opt.(t+1).(Hashtbl.find h w').(v);
     done;
   done;
-  !sum +. distribution.(id_w).(0).(s) *. opt.(t+1).(id_w).(v) (* distribution.(id_w).(0).(s) proba to go from w to w' with delta=s=0 (no work has arrived) *)
+  !sum
 
   
 (** MAIN FUNCTION *)
@@ -80,27 +80,26 @@ let () =
     let w = w_set.(k) in
     Hashtbl.add h (S.copy w) k; (* Giving a number to current w *)
     for i=1 to d do
-      for j=1 to s do
-        distribution.(k).(i-1).(j-1) <- Scanf.bscanf ffd "%f " (fun x -> x) (* Loading distribution of all the state we can go from w *)
+      for j=0 to s do
+        distribution.(k).(i-1).(j) <- Scanf.bscanf ffd "%f " (fun x -> x) (* Loading distribution of all the state we can go from w *)
       done;
     done;
-    distribution.(k).(0).(s) <- Scanf.bscanf ffd "%f\n" (fun x -> x); (* Here we store the case where s=0 *)
   done;
 
   (* Now, we can begin the main algorithm *)
   let t = ref (bt-1) in
 
-  while !t >= 1 do
+  while !t >= 0 do
     for k=0 to space-1 do (* We explore all the w *)
       let w = w_set.(k) in
       
       for i=0 to v_max do (* We explore all the speeds *)
-        let cost = ref ((Funs.f i v_max) +. (Funs.c v_max) +. (compute_sum w d s v_max opt distribution h !t)) in
+        let cost = ref ((Funs.f i v_max !t) +. (Funs.c v_max) +. (compute_sum w d s v_max opt distribution h !t)) in
         let p = ref v_max in
         for j=0 to v_max-1 do
           if j >= S.get w 1 then
             begin
-              let c = ((Funs.f i j) +. (Funs.c j) +. (compute_sum w d s j opt distribution h !t)) in
+              let c = ((Funs.f i j !t) +. (Funs.c j) +. (compute_sum w d s j opt distribution h !t)) in
               if c < !cost then
                 begin
                   cost := c;
@@ -116,39 +115,23 @@ let () =
     t := !t-1;
   done;
 
-  (* Case where t=0 *)
-  
-  for k=0 to space-1 do
-    let w' = S.copy w_set.(k) in
-    let cost = ref ((Funs.c v_max) +. (compute_sum w' d s v_max opt distribution h !t)) in
-    let p = ref v_max in
-    for i=0 to v_max-1 do
-      if i >= S.get w' 1 then
-        begin
-          let c = ((Funs.c i) +. (compute_sum w' d s v_max opt distribution h !t)) in
-          if c < !cost then
-            begin
-              cost := c;
-              p := i
-            end
-        end
-    done;
-  done;
   close_in fd;
 
   (* Output best policies array *)
+
   let fd = open_out Sys.argv.(4) in
 
-  for i=1 to bt do
-    for j=0 to space-2 do
+  for i=0 to bt do
+    for j=0 to space-1 do
       for k=0 to v_max do
-        fprintf fd "%d " pol.(i).(j).(k)
+        if j=space-1 && k=v_max then
+          fprintf fd "%d\n" pol.(i).(j).(k)
+        else
+          fprintf fd "%d " pol.(i).(j).(k)
       done;
     done;
-    for k=0 to v_max-1 do
-      fprintf "%d " pol.(i).(j
-    done;
   done;
+  
   close_out fd;
   
   printf "Bye !\n";
