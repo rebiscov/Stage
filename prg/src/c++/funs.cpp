f5c187898cb398d8494fada548b9878ad3b8db70
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-21 15:20:01 +0200

    funs for the cost

---
+++ prg/src/c++/funs.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+#include "funs.h"
+
+double f (unsigned int v1, unsigned int v2, unsigned int t){
+  if (t == 0)
+    return 0;
+  else
+    return 0;
+}
+
+
+double c(unsigned int v){
+  return exp((double)v);
+}
---
+++ prg/src/c++/funs.h
@@ -0,0 +1,7 @@
+#ifndef FUNS
+#define FUNS
+
+double f (unsigned int v1, unsigned int v2, unsigned int t);
+double c(unsigned int v);
+
+#endif
