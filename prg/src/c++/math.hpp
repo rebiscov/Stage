24bfefc4cbcb4ccffef590517e34b6af34613ae1
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-21 16:21:15 +0200

    change extension

---
+++ prg/src/c++/math.hpp
@@ -0,0 +1,3 @@
+unsigned int bino(unsigned int k, unsigned int n);
+unsigned int c(unsigned int d, unsigned int s);
+unsigned int max(unsigned int a, unsigned int b);
---
+++ prg/src/c++/states.hpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+#ifndef W_CLASS
+#define W_CLASS
+
+class W{
+ public:
+  
+  W();
+  W(unsigned int d);
+  unsigned int& operator[](unsigned int i);
+  void operator=(W obj);
+  bool operator<(const W& obj) const;
+  bool is_null(void);
+  void add_work(unsigned int delta, unsigned int sigma);
+  void print_w(void);
+  unsigned int get(unsigned int i);
+  void add(unsigned int i, unsigned int e);
+  void inc_time(unsigned int v);
+  void set(unsigned int x, unsigned int e);
+
+ private:
+  std::vector<unsigned int> w;
+};
+
+std::vector<W> compute_w(unsigned int d, unsigned int s);
+unsigned int state_space(unsigned int d, unsigned int s);
+
+#endif
