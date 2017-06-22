e936e9f24e833cc312afda0e97cd4e720ebe7b32
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-22 09:34:33 +0200

    delete files with wrong extension

--- prg/src/c++/funs.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNS
-#define FUNS
-
-double f (unsigned int v1, unsigned int v2, unsigned int t);
-double c(unsigned int v);
-
-#endif
--- prg/src/c++/math.h
+++
@@ -1,3 +0,0 @@
-unsigned int bino(unsigned int k, unsigned int n);
-unsigned int c(unsigned int d, unsigned int s);
-unsigned int max(unsigned int a, unsigned int b);
