85581a0b1a0fd26eba345a9b6e609da0bb2188b5
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-25 21:28:21 +0200

    more accurate with time

--- prg/src/c++/perf.cpp
+++ prg/src/c++/perf.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
+#include <ctime>
 #include <unordered_map>
 #include "states.hpp"
 
 int main(int argc, char *argv[]){
   unsigned int d, s, space;
+  clock_t t1, t2, t3;
 
   if (argc < 3){
     printf("Use %s d s\n", argv[0]);
@@ -14,11 +16,15 @@ int main(int argc, char *argv[]){
   s = atoi(argv[2]);
   space = state_space(d, s);
 
+  t1 = clock();
+
   std::unordered_map<W, unsigned int> h(space);
   std::vector<W> w_set = compute_w(d, s);
 
   for (unsigned int k = 0; k < space; k++)
     h[w_set[k]] = k;
+
+  t2 = clock();
 
   unsigned int a = 56;
 
@@ -27,6 +33,11 @@ int main(int argc, char *argv[]){
     if (key > a)
       a = key;
   }
+
+  t3 = clock();
+
+  printf("Time to compute w_set and the hashtable: %f\n", ((double)(t2-t1))/CLOCKS_PER_SEC);
+  printf("Time to access all the elements of the hashtable %f\n", (double)(t3-t2)/CLOCKS_PER_SEC);
 	
   return 0;
 }
