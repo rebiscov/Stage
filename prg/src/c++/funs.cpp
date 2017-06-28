4a33fd4cde6fa2a67617ed4b384dd070d77c2f79
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-28 15:03:51 +0200

    change f

--- prg/src/c++/funs.cpp
+++ prg/src/c++/funs.cpp
@@ -1,14 +1,21 @@
-#include <math.h>
+#include <cmath>
+#include <algorithm>
 #include "funs.hpp"
 
-double f (unsigned int v1, unsigned int v2, unsigned int t){
-  if (t == 0)
-    return 0;
-  else
-    return 0;
-}
-
+#define delta 0.1
 
 double c(unsigned int v){
   return (double)(v*v*v);
+}
+
+double f (unsigned int v1, unsigned int v2, unsigned int t){
+  if (t == 0 || v1 == v2)
+    return 0;
+  else{
+    unsigned int v_min = std::min(v1,v2);
+    unsigned int v_max = std::max(v1,v2);
+    double delta_1 = (delta*(double)v_min) / (double)std::abs((long)(v1-v2));
+    
+    return delta_1*(double)c(v_max)* - (delta + delta_1)*(double)c(v_min);
+  }
 }
