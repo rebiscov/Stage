f4590d12e79cfddf0e60783d88952a4b8df877dd
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-28 16:31:21 +0200

    change f

--- prg/src/funs.ml
+++ prg/src/funs.ml
@@ -1,8 +1,14 @@
-let f v1 v2 t=
-  if t=0 then
-    0.
-  else
-    0.
-
 let c v =
   float_of_int (v*v*v)
+
+let f v1 v2 t =
+  let delta = 0.1 in
+  if t=0 || v1 = v2 || v1 = 0 || v2 = 0 then
+    0.
+  else
+    begin
+      let v_min = min v1 v2 in
+      let v_max = max v1 v2 in
+      let delta_1 = (delta*.(float_of_int v_min)) /. ( float_of_int (v_max-v_min)) in
+      delta *. (c v_max) -. (delta +. delta_1)*.(c v_min)
+    end
