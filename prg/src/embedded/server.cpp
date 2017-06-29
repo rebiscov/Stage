ea0067b2ee8c932784cce0945ad8b707d7664d3f
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-29 15:58:14 +0200

    add server.cpp

---
+++ prg/src/embedded/server.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unordered_map>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "states.hpp"
+
+#define MAX 500
+#define PORT 1338
+
+int main(int argc, char *argv[]){
+
+  return 0;
+}
