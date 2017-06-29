055bda4b411f19da877e56dfb3c1efea6d7b6c89
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-29 15:08:02 +0200

    add possibility to just who avg changes and avg cost

--- prg/src/simu/print_simu.py
+++ prg/src/simu/print_simu.py
@@ -6,6 +6,10 @@ import sys
 if len(sys.argv) < 2:
     print("Use: ./" + sys.argv[0] + " values_file")
     exit(1)
+
+b=False
+if len(sys.argv) > 2:
+    b=True
     
 lines = []
 with open(sys.argv[1]) as file:
@@ -27,10 +31,11 @@ for i in range(int(len(num)/3)):
     cost=num[3*i][0]
     avg_cost += cost
     avg_changes += changes
-    plt.title('Cost: ' + str(cost) + ', number of changes: ' + str(changes) + '.')
-    plt.plot(x, num[3*i+1])
-    plt.step(x, num[3*i+2], where ='post')
-    plt.show()
+    if not b:
+        plt.title('Cost: ' + str(cost) + ', number of changes: ' + str(changes) + '.')
+        plt.plot(x, num[3*i+1])
+        plt.step(x, num[3*i+2], where ='post')
+        plt.show()
 
 n = len(num)/3
 print('Average cost = ' + str(avg_cost/n) + ', average number of changes = ' + str(avg_changes/n))
