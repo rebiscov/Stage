a09885fa16c64958ac293532a1cb81a347498f3d
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-06-30 10:30:26 +0200

    add ping/pong

--- prg/src/embedded/client.cpp
+++ prg/src/embedded/client.cpp
@@ -9,20 +9,64 @@
 #include <sys/socket.h>
 #include "states.hpp"
 
-#define MAX 500
+#define MAX 5000
 #define PORT 1337
+#define NB_PINGS 10
+
+void rcv_line(char* rcv, int sock);
+int pong(int sock);
 
 int main(int argc, char *argv[]){
-  int sock;
+  int sock, len;
   struct sockaddr_in dst;
   char rcv[MAX];
 
   sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1){
+    perror("sock()");
+    exit(EXIT_FAILURE);
+  }
 
   memset(&dst, 0, sizeof(dst));
   dst.sin_family = AF_INET;
   dst.sin_addr.s_addr = inet_addr("127.0.0.1");
   dst.sin_port = htons(PORT);
 
+  if (connect(sock, (struct sockaddr *)&dst, sizeof(sockaddr_in)) == -1){
+    perror("connect()");
+    exit(EXIT_FAILURE);
+  }
+
+  int n = pong(sock);
+
+  close(sock);
+
   return 0;
+}
+
+void rcv_line(char* rcv, int sock){
+  char* buff = rcv;
+  int len;
+  do{
+    len = recv(sock, rcv, MAX, 0);
+    buff += len;
+    buff[0] = '\0';
+  } while(buff[-1] != '\n');
+  
+  buff[0] = '\0';
+}
+
+int pong (int sock){
+  char buff[MAX];
+  int len, res;
+  for (unsigned int i = 0; i < NB_PINGS; i++){
+    recv(sock, buff, MAX, 0);
+    send(sock, "PONG", 4, 0);
+  }
+  len = recv(sock, buff, MAX, 0);
+  buff[len] = '\0';
+  res = atoi(buff);
+  printf("number of intervals = %d\n", res+1);
+  
+  return res + 1;
 }
--- prg/src/embedded/server.cpp
+++ prg/src/embedded/server.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <unordered_map>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -9,10 +10,70 @@
 #include <sys/socket.h>
 #include "states.hpp"
 
-#define MAX 500
-#define PORT 1338
+#define MAX 5000
+#define PORT 1337
+#define THRESHOLD 0.1
+#define ERROR 0.05
+#define NB_PINGS 10
+
+int ping(int conn);
 
 int main(int argc, char *argv[]){
+  struct sockaddr_in dst, srv;
+  int sock;
+  socklen_t socksize = sizeof(struct sockaddr_in);
+  char res[MAX];
 
+  memset(&srv, 0, sizeof(srv));
+  srv.sin_family = AF_INET;
+  srv.sin_addr.s_addr = htonl(INADDR_ANY);
+  srv.sin_port = htons(PORT);
+
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1){
+    perror("sock()");
+    exit(EXIT_FAILURE);
+  }
+
+  bind(sock, (struct sockaddr *)&srv, sizeof(struct sockaddr));
+
+  listen(sock, 1);
+  
+  int conn = accept(sock, (struct sockaddr *)&dst, &socksize);
+  if (conn == -1){
+    perror("accept()");
+    exit(EXIT_FAILURE);
+  }
+
+  int n = ping(conn);
+  
+  
+  close(conn);
+  close(sock);
+  
   return 0;
+}
+
+int ping(int conn){
+  double max = -1.;
+  clock_t t;
+  char buff[MAX];
+  int len, res;
+
+  for (unsigned int i = 0; i < NB_PINGS; i++){
+    t = clock();
+    send(conn, "PING", 4, 0);
+    len = recv(conn, buff, MAX, 0);
+    t = clock() - t;
+    if ((double)(t/CLOCKS_PER_SEC) > max)
+      max = (double)(t/CLOCKS_PER_SEC);
+  }
+  printf("Longest ping: %f\n", max);
+  max += ERROR;
+  res = (int)(max/THRESHOLD);
+  sprintf(buff, "%d", res);
+  send(conn, buff, strlen(buff), 0);
+  printf("number of intervals = %d\n", res+1);
+
+  return res + 1;
 }
