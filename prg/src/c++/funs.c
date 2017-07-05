333b165dbad537fd084481348275f6083d95f500
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-07-05 15:11:17 +0200

    add min_f and max_f

--- prg/src/c++/funs.c
+++ prg/src/c++/funs.c
@@ -1,7 +1,18 @@
 #include <math.h>
 #include <stdio.h>
 #include "funs.h"
-#include "math.h"
+
+unsigned int max_f(unsigned int a, unsigned int b){
+  if (a >= b)
+    return a;
+  return b;
+}
+
+unsigned int min_f(unsigned int a, unsigned int b){
+  if (a <= b)
+    return a;
+  return b;
+}
 
 double c(unsigned int v){
   return (double)(v*v);
@@ -12,8 +23,8 @@ double f (unsigned int v1, unsigned int v2, unsigned int t){
   if (t == 0 || v1 == v2 || v1 == 0 || v2 == 0)
     return 0;
   else{
-    unsigned int v_min = min(v1, v2);
-    unsigned int v_max = max(v1,v2);
+    unsigned int v_min = min_f(v1, v2);
+    unsigned int v_max = max_f(v1,v2);
     double delta_1 = (double)(delta*(double)v_min) / (double)(v_max-v_min);
     double res = delta_1*c(v_max) - (delta + delta_1)*c(v_min) + delta*((1/3)*(pow((double)v_max, 3.) - pow((double)v_min, 3.)));
     
