857b247ffdf717833e787be9b6c668ad51103207
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-07-05 15:20:24 +0200

    communicate with fun_c.c

--- prg/src/funs.ml
+++ prg/src/funs.ml
@@ -1,14 +1,8 @@
+external cost : int -> float = "cost"
+external overcost : int -> int -> int -> float = "overcost"
+
 let c v =
-  float_of_int (v*v*v)
+  cost v
 
 let f v1 v2 t =
-  let delta = 0.1 in
-  if t=0 || v1 = v2 || v1 = 0 || v2 = 0 then
-    0.
-  else
-    begin
-      let v_min = min v1 v2 in
-      let v_max = max v1 v2 in
-      let delta_1 = (delta*.(float_of_int v_min)) /. ( float_of_int (v_max-v_min)) in
-      delta *. (c v_max) -. (delta +. delta_1)*.(c v_min) +. delta *. (float_of_int (v1 + v2)) /. 2.
-    end
+  overcost v1 v2 t
